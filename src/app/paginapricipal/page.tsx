13cc186b7a0986bb889e45970e5722f703f5fdf6
Jefferson Reginaldo <141447493+Jefferson667@users.noreply.github.com>
2023-10-08 13:03:57 -0300

    ok

--- src/app/paginapricipal/page.tsx
+++ src/app/paginapricipal/page.tsx
@@ -1,14 +1,19 @@
+import Image from "next/image"
+
 export default function Pagina1() {
   return (
     <>
       <main>
-        <image
-          width={100}
-          height={100}
-          src="/cunha.jpeg"
-          alt=""
+        <header className="justify-center flex">
+          <Image
+            width={300}
+            height={300}
+            src="/pratocunha.jpeg"
+            alt="error"
+          />
+        </header>
 
-        />
+
       </main>
 
 
