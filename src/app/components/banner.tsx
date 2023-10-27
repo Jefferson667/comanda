c4a45116bf084978ad9728895f466b72972a63e9
Jefferson Reginaldo <141447493+Jefferson667@users.noreply.github.com>
2023-10-27 19:27:13 -0300

    ghj

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function Banner() {
     return (
         <>
-            <Image
+            <Image className="flex w-full"
                 width={1100}
                 height={100}
                 src={"/card.jpg"}
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -5,22 +5,14 @@ export default function Header() {
     return (
         <>
             <main>
-                <header className="flex justify-between bg-gray-100">
+                <header className="flex justify-center bg-gray-100">
 
                     <Image
-                        width={100}
-                        height={100}
+                        width={150}
+                        height={150}
                         src="/don.jpg"
                         alt="error"
                     />
-
-                    <nav className="flex items-center mr-2  font-bold">
-                        <p>
-                           <Link href="marmitas">mm</Link> 
-                        </p>
-
-
-                    </nav>
 
                 </header>
 
